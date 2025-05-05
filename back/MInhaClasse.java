a0b6736554d931f9926fe2318bb113c655095775
Hisabela da Rocha Campos <hisabeladarocha.campos@philips.com>
2025-05-05 15:49:59 -0300

    [SO-123456] New class added

---
+++ back/MInhaClasse.java
@@ -0,0 +1,8 @@
+# Hello-World
+Repositório criado para o nosso treinamento sobre o github
+# Hisabela Campos
+Estamos estudando github a partir do material disponível por ele
+
+        public MinhaClasse{
+
+        }
